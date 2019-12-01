1e77bcfb51ca624f3520d82434fb9c7acb8ac52d
Carlos Francisco <35729190+CarlosFco@users.noreply.github.com>
2019-12-01 11:16:20 +0100

    Icon changed (not def)

--- Temp/gradleOut/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/URJC/TFG/R.java
+++ Temp/gradleOut/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/URJC/TFG/R.java
@@ -11,8 +11,7 @@ public final class R {
   public static final class mipmap {
     public static final int app_icon=0x7f010000;
     public static final int app_icon_round=0x7f010001;
-    public static final int ic_launcher_background=0x7f010002;
-    public static final int ic_launcher_foreground=0x7f010003;
+    public static final int ic_launcher_foreground=0x7f010002;
   }
   public static final class string {
     public static final int app_name=0x7f020000;
