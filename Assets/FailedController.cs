9833436b8c375c0c267c40873959998e00f7a03b
Carlos Francisco <35729190+CarlosFco@users.noreply.github.com>
2020-01-13 16:49:36 +0100

    Doing some cleaning

--- Assets/FailedController.cs
+++ Assets/FailedController.cs
@@ -1,16 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FailedController : MonoBehaviour
 {
 
     public GameObject failedoption;
     public Animator failed;
-
-    public float timer = 0;
-    public float timerMax = 0;
-    public bool reset = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +16,6 @@ public class FailedController : MonoBehaviour
         failed = failedoption.GetComponent<Animator>();
         failed.speed = 0f;
     }
-
-    private void Update()
-    {
-        
-    }
-
 
     public void Failed()
     {
@@ -38,31 +29,9 @@ public class FailedController : MonoBehaviour
         failed.speed = 0f;
     }
 
-    IEnumerator Coroutine()
+    public void Done()
     {
-        //Print the time of when the function is first called.
-        Debug.Log("Started Coroutine at timestamp : " + Time.time);
-        failed.speed = 1f;
-        //yield on a new YieldInstruction that waits for 2 seconds.
-        yield return new WaitForSeconds(2);
-        failed.speed = 0f;
-        StopAllCoroutines();
-        //After we have waited 2 seconds print the time again.
-        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        SceneManager.LoadScene("ChooseWay", LoadSceneMode.Single);
     }
 
-    private bool Waited(float seconds)
-    {
-        timerMax = seconds;
-
-        timer += Time.deltaTime;
-
-        if (timer >= timerMax)
-        {
-            reset = true;
-            return true; //max reached - waited x - seconds
-        }
-
-        return false;
-    }
 }
