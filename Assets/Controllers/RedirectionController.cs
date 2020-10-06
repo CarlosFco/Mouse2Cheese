050da959eb624953eef5d07ed83306c9c5248ec3
Carlos Francisco <35729190+CarlosFco@users.noreply.github.com>
2020-10-06 10:58:59 +0200

    Re-organized folders and minor fixes

---
+++ Assets/Controllers/RedirectionController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RedirectionController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+
+    public void ToChooseWay()
+    {
+        SceneManager.LoadScene("ChooseWay", LoadSceneMode.Single);
+    }
+
+    public void ToRecognition()
+    {
+        SceneManager.LoadScene("Reco", LoadSceneMode.Single);
+    }
+
+    public void ToHowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlayScreen", LoadSceneMode.Single);
+    }
+
+    public void BackToChooseWay()
+    {
+        SceneManager.LoadScene("ChooseWay", LoadSceneMode.Single);
+    }
+
+    public void ToLevel3Question()
+	{
+        SceneManager.LoadScene("Level3Question", LoadSceneMode.Single);
+    }
+
+    public void ToLevel4Question()
+	{
+        SceneManager.LoadScene("Level4Question", LoadSceneMode.Single);
+    }
+
+    public void ToLevel5Question()
+    {
+        SceneManager.LoadScene("Level5Question", LoadSceneMode.Single);
+    }
+
+    public void ToLevel6Question()
+    {
+        SceneManager.LoadScene("Level6Question", LoadSceneMode.Single);
+    }
+
+}
---
+++ Assets/Recognitions/Level2Recognition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Vuforia;
+
+public class Level2Recognition : MonoBehaviour, ITrackableEventHandler
+{
+    TrackableBehaviour mTrackableBehaviour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED || newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            print("DETECTA");
+            SceneManager.LoadScene("Level2Scene", LoadSceneMode.Single);
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED && newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            print("PIERDE");
+        }
+    }
+
+}
